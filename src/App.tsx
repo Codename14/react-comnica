c495bec84fdad174cfe8d772f98b85d5a39a5951
Alex Banai <46088872+Codename14@users.noreply.github.com>
2024-10-31 16:53:52 +0100

    adding webp background

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import Button from './components/Button';
-import HeroIMG from './assets/images/hero.jpg';
+// import HeroIMG from './assets/images/laptop.webp';
+import HeroIMG from './assets/images/hero.webp';
+import BackgroungIMG from './assets/images/bg.webp';
 
 function App() {
     return (
@@ -12,10 +14,12 @@ function App() {
                                 <h2 className='hero-title color-primary'>Comnica Signature</h2>
                                 <h4 className='text-bold '>Digitális aláíró szolgáltatás a Comnicától</h4>
                             </div>
-                            <img className='hero__img' src={HeroIMG} alt='' />
+                            <img className='hero__img' src={HeroIMG} alt='illustration' />
                         </div>
+                        <img className='bg__image left' src={BackgroungIMG} alt='abstract lines' />
                     </div>
                     <div className='hero__right'>
+                        <img className='bg__image right' src={BackgroungIMG} alt='abstract lines' />
                         <div>
                             <h2 className='hero-title color-secondary '>
                                 Üdvözlöm <br />a Comnica Signature-ben!
